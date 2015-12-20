af25a90297f7c0e042944e9335d377a652c69ae6
Michael Tuexen <tuexen@fh-muenster.de>
2015-12-20 21:08:43 +0100

    Add a routine for selecting the transport protocol based on flow parameters.

--- neat_he.c
+++ neat_he.c
@@ -83,6 +83,109 @@ he_resolve_cb(struct neat_resolver *resolver, struct neat_resolver_results *resu
                 flow->family, flow->sockType, flow->sockProtocol, -1);
 }
 
+static uint8_t neat_he_transport_protocols(uint64_t propertyMask,
+                                           uint8_t protocols[])
+{
+    uint8_t nr_of_protocols;
+
+    nr_of_protocols = 0;
+
+    /* Check for stupid settings */
+    if ((propertyMask & NEAT_PROPERTY_SCTP_BANNED) &&
+        (propertyMask & NEAT_PROPERTY_TCP_BANNED) &&
+        (propertyMask & NEAT_PROPERTY_UDP_BANNED) &&
+        (propertyMask & NEAT_PROPERTY_UDPLITE_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_SCTP_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_SCTP_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_TCP_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_TCP_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_UDP_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_UDP_BANNED))
+        return nr_of_protocols;
+    if ((propertyMask & NEAT_PROPERTY_UDPLITE_REQUIRED) &&
+        (propertyMask & NEAT_PROPERTY_UDPLITE_BANNED))
+        return nr_of_protocols;
+
+    /* Check explicit protocol requests first */
+    if (propertyMask & NEAT_PROPERTY_SCTP_REQUIRED) {
+        if (((propertyMask & NEAT_PROPERTY_TCP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDPLITE_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_BANNED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_SCTP;
+        return nr_of_protocols;
+    }
+    if (propertyMask & NEAT_PROPERTY_TCP_REQUIRED) {
+        if (((propertyMask & NEAT_PROPERTY_SCTP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDPLITE_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_MESSAGE) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_BANNED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_BANNED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_TCP;
+        return nr_of_protocols;
+    }
+    if (propertyMask & NEAT_PROPERTY_UDP_REQUIRED) {
+        if (((propertyMask & NEAT_PROPERTY_SCTP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_TCP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDPLITE_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_UDP;
+        return nr_of_protocols;
+    }
+    if (propertyMask & NEAT_PROPERTY_UDPLITE_REQUIRED) {
+        if (((propertyMask & NEAT_PROPERTY_SCTP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_TCP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_UDP_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_UDPLITE;
+        return nr_of_protocols;
+    }
+
+    /* Finally the more complex part */
+    if (propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED) {
+        if ((propertyMask & NEAT_PROPERTY_SCTP_BANNED) == 0)
+            protocols[nr_of_protocols++] = IPPROTO_SCTP;
+        if (((propertyMask & NEAT_PROPERTY_MESSAGE) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_BANNED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_TCP_BANNED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_TCP;
+    } else if (propertyMask & NEAT_PROPERTY_CONGESTION_CONTROL_BANNED) {
+        if ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED) == 0) {
+            if ((propertyMask & NEAT_PROPERTY_UDP_BANNED) == 0)
+                protocols[nr_of_protocols++] = IPPROTO_UDP;
+            if ((propertyMask & NEAT_PROPERTY_UDPLITE_BANNED) == 0)
+                protocols[nr_of_protocols++] = IPPROTO_UDPLITE;
+        }
+    } else {
+        if ((propertyMask & NEAT_PROPERTY_SCTP_BANNED) == 0)
+            protocols[nr_of_protocols++] = IPPROTO_SCTP;
+        if (((propertyMask & NEAT_PROPERTY_MESSAGE) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_BANNED) == 0) &&
+            ((propertyMask & NEAT_PROPERTY_TCP_BANNED) == 0))
+            protocols[nr_of_protocols++] = IPPROTO_TCP;
+        if ((propertyMask & NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED) == 0) {
+            if ((propertyMask & NEAT_PROPERTY_UDP_BANNED) == 0)
+                protocols[nr_of_protocols++] = IPPROTO_UDP;
+            if ((propertyMask & NEAT_PROPERTY_UDPLITE_BANNED) == 0)
+                protocols[nr_of_protocols++] = IPPROTO_UDPLITE;
+        }
+    }
+
+    return nr_of_protocols;
+}
+
 neat_error_code neat_he_lookup(neat_ctx *ctx, neat_flow *flow, neat_he_callback_fx callback_fx)
 {
     int protocol;

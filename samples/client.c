3e0394afa8c9a638abb33ae0fc312604a5cda6b0
Felix Weinrank <info@weinrank.net>
2016-01-27 14:34:27 +0100

    client - extend functionality, sync usage with tneat

--- samples/client.c
+++ samples/client.c
@@ -6,22 +6,18 @@
 #include "../neat.h"
 
 /*
-    Simple neat client for echo, discard and daytime server
+    Simple neat client
 */
 
-//#define NO_DEBUG_INFO
-#define BUFFERSIZE 32
-
-#ifdef NO_DEBUG_INFO
-#define debug_info(M, ...)
-#else
-#define debug_info(M, ...) fprintf(stderr, "[INFO][%s:%d] " M "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#endif
+static uint32_t config_rcv_buffer_size = 256;
+static uint32_t config_snd_buffer_size = 8;
+static uint16_t config_log_level = 1;
+static char config_property[] = "NEAT_PROPERTY_TCP_REQUIRED,NEAT_PROPERTY_IPV4_REQUIRED";
 
 #define debug_error(M, ...) fprintf(stderr, "[ERROR][%s:%d] " M "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
 struct std_buffer {
-    unsigned char buffer[BUFFERSIZE];
+    unsigned char *buffer;
     uint32_t buffer_filled;
 };
 
@@ -31,9 +27,24 @@ struct neat_ctx *ctx;
 struct neat_flow *flow;
 uv_loop_t *uv_loop;
 uv_tty_t tty;
+static unsigned char *buffer_rcv;
+static unsigned char *buffer_snd;
 
 void tty_read(uv_stream_t *stream, ssize_t bytes_read, const uv_buf_t *buffer);
 void tty_alloc(uv_handle_t *handle, size_t suggested, uv_buf_t *buf);
+
+/*
+    print usage and exit
+*/
+static void print_usage() {
+    printf("client [OPTIONS] HOST PORT\n");
+    printf("\t- R \treceive buffer in byte (%d)\n", config_rcv_buffer_size);
+    printf("\t- S \tsend buffer in byte (%d)\n", config_snd_buffer_size);
+    printf("\t- v \tlog level 0..2 (%d)\n", config_log_level);
+    printf("\t- P \tneat properties (%s)\n", config_property);
+
+    exit(EXIT_FAILURE);
+}
 
 /*
     Error handler
@@ -47,14 +58,15 @@ static uint64_t on_error(struct neat_flow_operations *opCB) {
 */
 static uint64_t on_readable(struct neat_flow_operations *opCB) {
     // data is available to read
-    unsigned char buffer[BUFFERSIZE];
-    uint32_t buffered_amount;
+    uint32_t buffer_filled;
     neat_error_code code;
 
-    code = neat_read(opCB->ctx, opCB->flow, buffer, BUFFERSIZE, &buffered_amount);
+    code = neat_read(opCB->ctx, opCB->flow, buffer_rcv, config_rcv_buffer_size, &buffer_filled);
     if (code) {
         if (code == NEAT_ERROR_WOULD_BLOCK) {
-            debug_info("NEAT_ERROR_WOULD_BLOCK");
+            if (config_log_level >= 1) {
+                printf("on_readable - NEAT_ERROR_WOULD_BLOCK\n");
+            }
             return 0;
         } else {
             debug_error("code: %d", (int)code);
@@ -62,13 +74,19 @@ static uint64_t on_readable(struct neat_flow_operations *opCB) {
         }
     }
 
-    if (buffered_amount > 0) {
-        debug_info("got some data - %d byte", buffered_amount);
-        fwrite(buffer, sizeof(char), buffered_amount, stdout);
+    if (buffer_filled > 0) {
+        if (config_log_level >= 1) {
+            printf("data received - %d byte\n", buffer_filled);
+        }
+
+        fwrite(buffer_rcv, sizeof(char), buffer_filled, stdout);
         printf("\n");
         fflush(stdout);
+
     } else {
-        debug_info("buffered_amount is <= 0 - neat_stop_event_loop()");
+        if (config_log_level >= 1) {
+            printf("buffer_filled <= 0 - neat_stop_event_loop()\n");
+        }
         ops.on_readable = NULL;
         neat_stop_event_loop(opCB->ctx);
     }
@@ -87,7 +105,10 @@ static uint64_t on_writable(struct neat_flow_operations *opCB) {
         return on_error(opCB);
     }
 
-    debug_info("sent %d bytes", stdin_buffer.buffer_filled);
+    if (config_log_level >= 1) {
+        printf("sent %d bytes\n", stdin_buffer.buffer_filled);
+    }
+
     // stop writing
     opCB->on_writable = NULL;
     // data sent - continue reading from stdin
@@ -97,23 +118,26 @@ static uint64_t on_writable(struct neat_flow_operations *opCB) {
 
 
 static uint64_t on_connected(struct neat_flow_operations *opCB) {
-    debug_info();
+    if (config_log_level >= 1) {
+        printf("connected\n");
+    }
 
     opCB->on_readable = on_readable;
-    //opCB->on_writable = on_writable;
     return 0;
 }
 
 /*
     Read from stdin
 */
-void tty_read(uv_stream_t *stream, ssize_t bytes_read, const uv_buf_t *buffer) {
-    if (bytes_read > 0) {
-        debug_info("read %d bytes from stdin", (int) bytes_read);
+void tty_read(uv_stream_t *stream, ssize_t buffer_filled, const uv_buf_t *buffer) {
+    if (buffer_filled > 0) {
+        if (config_log_level >= 1) {
+            printf("read %d bytes from stdin\n", (int) buffer_filled);
+        }
 
         // copy input to app buffer
-        stdin_buffer.buffer_filled = bytes_read;
-        memcpy(stdin_buffer.buffer, buffer->base, bytes_read);
+        stdin_buffer.buffer_filled = buffer_filled;
+        memcpy(stdin_buffer.buffer, buffer->base, buffer_filled);
 
         // stop reading from stdin and set write callback
         uv_read_stop(stream);
@@ -123,21 +147,59 @@ void tty_read(uv_stream_t *stream, ssize_t bytes_read, const uv_buf_t *buffer) {
 }
 
 void tty_alloc(uv_handle_t *handle, size_t suggested, uv_buf_t *buf) {
-    buf->len = BUFFERSIZE;
-    buf->base = malloc(BUFFERSIZE);
+    buf->len = config_rcv_buffer_size;
+    buf->base = malloc(config_rcv_buffer_size);
 }
 
 int main(int argc, char *argv[]) {
+    uint64_t prop;
+    int arg;
+    char *arg_property = config_property;
+    char *arg_property_ptr;
+    char arg_property_delimiter[] = ",;";
     ctx = neat_init_ctx();
     uv_loop = neat_get_uv_loop(ctx);
-    uint64_t prop;
 
-    // check for argumets
-    if (argc != 3) {
-        fprintf(stderr, "Insufficient arguments!\n");
-        fprintf(stderr, "Usage: %s ADDRESS PORT\n",argv[0]);
-        exit(EXIT_FAILURE);
+    while ((arg = getopt(argc, argv, "R:S:v:P:")) != -1) {
+		switch(arg) {
+            case 'R':
+                config_rcv_buffer_size = atoi(optarg);
+                if (config_log_level >= 1) {
+                    printf("option - receive buffer size: %d\n", config_rcv_buffer_size);
+                }
+                break;
+            case 'S':
+                config_snd_buffer_size = atoi(optarg);
+                if (config_log_level >= 1) {
+                    printf("option - send buffer size: %d\n", config_snd_buffer_size);
+                }
+                break;
+            case 'v':
+                config_log_level = atoi(optarg);
+                if (config_log_level >= 1) {
+                    printf("option - log level: %d\n", config_log_level);
+                }
+                break;
+            case 'P':
+                arg_property = optarg;
+                if (config_log_level >= 1) {
+                    printf("option - properties: %s\n", arg_property);
+                }
+                break;
+            default:
+                print_usage();
+                break;
+        }
     }
+
+    if (optind + 2 != argc) {
+        debug_error("argument error");
+        print_usage();
+    }
+
+    buffer_rcv = malloc(config_rcv_buffer_size);
+    buffer_snd = malloc(config_snd_buffer_size);
+    stdin_buffer.buffer = malloc(config_snd_buffer_size);
 
     if (ctx == NULL) {
         debug_error("could not initialize context");
@@ -146,7 +208,6 @@ int main(int argc, char *argv[]) {
 
     uv_tty_init(uv_loop, &tty, 0, 1);
     uv_read_start((uv_stream_t*) &tty, tty_alloc, tty_read);
-
 
     // new neat flow
     if((flow = neat_new_flow(ctx)) == NULL) {
@@ -159,9 +220,63 @@ int main(int argc, char *argv[]) {
         debug_error("neat_get_property");
         exit(EXIT_FAILURE);
     }
-    //prop |= NEAT_PROPERTY_OPTIONAL_SECURITY;
-    prop |= NEAT_PROPERTY_TCP_REQUIRED; /* FIXME: Remove this once HE works */
-    //prop |= NEAT_PROPERTY_UDP_REQUIRED;
+
+    // read property arguments
+    arg_property_ptr = strtok(arg_property, arg_property_delimiter);
+
+    while (arg_property_ptr != NULL) {
+        if (config_log_level >= 1) {
+            printf("setting property: %s\n", arg_property_ptr);
+        }
+
+        if (strcmp(arg_property_ptr,"NEAT_PROPERTY_OPTIONAL_SECURITY") == 0) {
+            prop |= NEAT_PROPERTY_TCP_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_REQUIRED_SECURITY") == 0) {
+            prop |= NEAT_PROPERTY_REQUIRED_SECURITY;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_MESSAGE") == 0) {
+            prop |= NEAT_PROPERTY_MESSAGE;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_IPV4_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_IPV4_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_IPV4_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_IPV4_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_IPV6_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_IPV6_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_IPV6_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_IPV6_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_SCTP_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_SCTP_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_SCTP_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_SCTP_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_TCP_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_TCP_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_TCP_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_TCP_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_UDP_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_UDP_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_UDP_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_UDP_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_UDPLITE_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_UDPLITE_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_UDPLITE_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_UDPLITE_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_CONGESTION_CONTROL_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_CONGESTION_CONTROL_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_CONGESTION_CONTROL_BANNED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED") == 0) {
+            prop |= NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED;
+        } else if (strcmp(arg_property_ptr,"NEAT_PROPERTY_RETRANSMISSIONS_BANNED") == 0) {
+            prop |= NEAT_PROPERTY_RETRANSMISSIONS_BANNED;
+        } else {
+            printf("error - unknown property: %s\n", arg_property_ptr);
+            print_usage();
+        }
+
+    	// get next property
+     	arg_property_ptr = strtok(NULL, arg_property_delimiter);
+    }
+
+    // set properties
     if (neat_set_property(ctx, flow, prop)) {
         debug_error("neat_set_property");
         exit(EXIT_FAILURE);
@@ -177,16 +292,16 @@ int main(int argc, char *argv[]) {
     }
 
     // wait for on_connected or on_error to be invoked
-    if (neat_open(ctx, flow, argv[1], argv[2])) {
+    if (neat_open(ctx, flow, argv[argc - 2], argv[argc - 1])) {
         debug_error("neat_open");
         exit(EXIT_FAILURE);
     }
 
-
-
-
-
     neat_start_event_loop(ctx, NEAT_RUN_DEFAULT);
+
+    free(buffer_rcv);
+    free(buffer_snd);
+    free(stdin_buffer.buffer);
 
     // cleanup
     neat_free_flow(flow);

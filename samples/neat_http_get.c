eeaaaa816d69f769be4ca3ee4146427275f5dd90
Felix Weinrank <weinrank@fh-muenster.de>
2016-04-15 14:13:26 +0200

    http_get refactoring

--- samples/neat_http_get.c
+++ samples/neat_http_get.c
@@ -3,68 +3,54 @@
 #include <string.h>
 #include "../neat.h"
 
-// clang -g neat_basic.c ../build/libneatS.a -luv -lldns -lmnl
-
-/*
- This is a very simple example of a basic client application.
- Connect by name, write out some data, and read some data while
- printing that to stdout.
-*/
 
 static struct neat_flow_operations ops;
+static uint32_t config_rcv_buffer_size = 1024;
+static const char *request = "GET / HTTP/1.0\r\nUser-agent: libneat\r\nConnection: close\r\n\r\n";
 
-static neat_error_code
-on_error(struct neat_flow_operations *opCB)
+static neat_error_code on_error(struct neat_flow_operations *opCB)
 {
-    fprintf(stderr,"unexpected error\n");
-    exit (1);
-    return 0;
+    fprintf(stderr, "%s\n", __FUNCTION__);
+    exit(EXIT_FAILURE);
 }
 
-static neat_error_code
-on_readable(struct neat_flow_operations *opCB)
+static neat_error_code on_readable(struct neat_flow_operations *opCB)
 {
     // data is available to read
-    unsigned char buffer[32];
-    uint32_t amt;
+    unsigned char buffer[config_rcv_buffer_size];
+    uint32_t bytes_read;
     neat_error_code code;
 
-    code = neat_read(opCB->ctx, opCB->flow, buffer, 32, &amt);
+    code = neat_read(opCB->ctx, opCB->flow, buffer, config_rcv_buffer_size, &bytes_read);
     if (code == NEAT_ERROR_WOULD_BLOCK) {
         return 0;
-    }
-    if (code != NEAT_OK) {
+    } else if (code != NEAT_OK) {
         return on_error(opCB);
     }
-    if (!amt) { // eof
+
+    if (!bytes_read) { // eof
         fflush(stdout);
         ops.on_readable = NULL; // do not read more
         neat_set_operations(opCB->ctx, opCB->flow, &ops);
         neat_stop_event_loop(opCB->ctx);
-    } else if (amt > 0) {
-        fwrite(buffer, 1, amt, stdout);
+    } else if (bytes_read > 0) {
+        fwrite(buffer, sizeof(char), bytes_read, stdout);
     }
     return 0;
 }
 
-static const char *request =
-    "GET / HTTP/1.0\r\nUser-agent: libneat\r\nConnection: close\r\n\r\n";
 
-static neat_error_code
-on_all_written(struct neat_flow_operations *opCB)
+static neat_error_code on_all_written(struct neat_flow_operations *opCB)
 {
     ops.on_readable = on_readable;
     neat_set_operations(opCB->ctx, opCB->flow, &ops);
     return 0;
 }
 
-static neat_error_code
-on_writable(struct neat_flow_operations *opCB)
+static neat_error_code on_writable(struct neat_flow_operations *opCB)
 {
     neat_error_code code;
-    code = neat_write(opCB->ctx, opCB->flow,
-                      (const unsigned char *)request,
-                      strlen(request));
+    code = neat_write(opCB->ctx, opCB->flow, (const unsigned char *)request, strlen(request));
     if (code != NEAT_OK) {
         return on_error(opCB);
     }
@@ -73,17 +59,16 @@ on_writable(struct neat_flow_operations *opCB)
     return 0;
 }
 
-static neat_error_code
-on_connected(struct neat_flow_operations *opCB)
+static neat_error_code on_connected(struct neat_flow_operations *opCB)
 {
     // now we can start writing
     ops.on_writable = on_writable;
+    ops.on_all_written = on_all_written;
     neat_set_operations(opCB->ctx, opCB->flow, &ops);
     return 0;
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     struct neat_ctx *ctx = NULL;
     struct neat_flow *flow = NULL;
@@ -103,11 +88,13 @@ main(int argc, char *argv[])
         result = EXIT_FAILURE;
         goto cleanup;
     }
+
     if ((flow = neat_new_flow(ctx)) == NULL) {
         fprintf(stderr, "could not initialize context\n");
         result = EXIT_FAILURE;
         goto cleanup;
     }
+
     neat_get_property(ctx, flow, &prop);
     prop |= NEAT_PROPERTY_OPTIONAL_SECURITY;
     prop |= NEAT_PROPERTY_TCP_REQUIRED; /* FIXME: Remove this once HE works */
@@ -115,7 +102,6 @@ main(int argc, char *argv[])
 
     ops.on_connected = on_connected;
     ops.on_error = on_error;
-    ops.on_all_written = on_all_written;
     neat_set_operations(ctx, flow, &ops);
 
     // wait for on_connected or on_error to be invoked
@@ -125,6 +111,7 @@ main(int argc, char *argv[])
         fprintf(stderr, "Could not open flow\n");
         result = EXIT_FAILURE;
     }
+    
 cleanup:
     if (flow != NULL) {
         neat_free_flow(flow);

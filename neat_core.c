0bf17a15c43b88a499409879f15544844dacabfd
Øystein Dale <oystedal@ifi.uio.no>
2016-05-19 15:33:15 +0200

    Added OOM check on allocation of property list

--- neat_core.c
+++ neat_core.c
@@ -1726,6 +1726,10 @@ neat_flow *neat_new_flow(neat_ctx *mgr)
     rv->shutdownfx = neat_shutdown_via_kernel;
     TAILQ_INIT(&rv->bufferedMessages);
     rv->property_requests = malloc(sizeof(rv->property_requests));
+    if (!rv->property_requests) {
+        free(rv);
+        return NULL;
+    }
     neat_properties_init(rv);
 #if defined(USRSCTP_SUPPORT)
     rv->sock = NULL;

0a35c49d896ce8ffa60c5d36358e18ca3b303774
Øystein Dale <oystedal@ifi.uio.no>
2016-05-19 14:41:17 +0200

    Added get/set functions for properties with values

--- neat_api.c
+++ neat_api.c
@@ -19,6 +19,124 @@ void neat_request_property(neat_flow* flow, char* property) {
     LIST_INSERT_HEAD(flow->property_requests, prop, property_list);
 }
 
+void neat_set_property_bool(neat_flow* flow, char* property, int value)
+{
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            prop->type = TYPE_BOOL;
+            prop->data.int_value = value == 0 ? 0 : 1;
+            return;
+        }
+    }
+
+    prop = malloc(sizeof(*prop));
+    prop->property = strdup(property);
+    prop->type = TYPE_BOOL;
+    prop->data.int_value = value == 0 ? 0 : 1;
+
+    LIST_INSERT_HEAD(flow->property_requests, prop, property_list);
+}
+
+neat_error_code neat_get_property_bool(neat_flow* flow, char* property, int* value) {
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            if (prop->type != TYPE_BOOL)
+                return NEAT_ERROR_BAD_ARGUMENT;
+
+            *value = prop->data.int_value;
+            return NEAT_OK;
+        }
+    }
+
+    return NEAT_ERROR_BAD_ARGUMENT;
+}
+
+void neat_set_property_int(neat_flow* flow, char* property, int value)
+{
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            prop->type = TYPE_INT;
+            prop->data.int_value = value;
+            return;
+        }
+    }
+
+    prop = malloc(sizeof(*prop));
+    prop->property = strdup(property);
+    prop->type = TYPE_INT;
+    prop->data.int_value = value;
+
+    LIST_INSERT_HEAD(flow->property_requests, prop, property_list);
+}
+
+neat_error_code neat_get_property_int(neat_flow* flow, char* property, int* value) {
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            *value = prop->data.int_value;
+            return NEAT_OK;
+        }
+    }
+
+    return NEAT_ERROR_BAD_ARGUMENT;
+}
+
+void neat_set_property_float(neat_flow* flow, char* property, float value)
+{
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            prop->type = TYPE_FLOAT;
+            prop->data.float_value = value;
+            return;
+        }
+    }
+
+    prop = malloc(sizeof(*prop));
+    prop->property = strdup(property);
+    prop->type = TYPE_FLOAT;
+    prop->data.int_value = value;
+
+    LIST_INSERT_HEAD(flow->property_requests, prop, property_list);
+}
+
+void neat_set_property_string(neat_flow* flow, char* property, const char* value)
+{
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            prop->type = TYPE_STRING;
+            prop->data.string_value = strdup(value);
+            return;
+        }
+    }
+
+    prop = malloc(sizeof(*prop));
+    prop->property = strdup(property);
+    prop->type = TYPE_STRING;
+    prop->data.string_value = strdup(value);
+
+    LIST_INSERT_HEAD(flow->property_requests, prop, property_list);
+}
+
+neat_error_code neat_get_property_string(neat_flow* flow, char* property, char** value) {
+    struct neat_prop_request* prop = NULL;
+    LIST_FOREACH(prop, flow->property_requests, property_list) {
+        if (strcmp(prop->property, property) == 0) {
+            if (prop->type != TYPE_STRING)
+                return NEAT_ERROR_BAD_ARGUMENT;
+
+            *value = strdup(prop->data.string_value);
+            return NEAT_OK;
+        }
+    }
+
+    return NEAT_ERROR_BAD_ARGUMENT;
+}
+
 void neat_remove_property(neat_flow* flow, char* property) {
     struct neat_prop_request* prop = NULL;
     LIST_FOREACH(prop, flow->property_requests, property_list) {
@@ -50,4 +168,16 @@ int neat_check_property(neat_flow* flow, char* property) {
     }
 
     return 0;
+}
+
+// Initialize the property set to the defaults
+void neat_properties_init(neat_flow* flow) {
+    neat_set_property_bool(flow, "seamless_handover", 0);
+    neat_set_property_bool(flow, "optimise_continuous_connectivity", 0);
+    neat_set_property_int(flow, "metadata_privacy", 0);
+    neat_set_property_bool(flow, "disable_dynamic_enhancement", 0);
+    neat_set_property_bool(flow, "low_latency_desired", 0);
+    neat_set_property_int(flow, "flow_group", 0);
+    neat_set_property_float(flow, "flow_priority", 0.5f);
+    neat_set_property_float(flow, "dscp_value", 0.5f);
 }

aa8e32b938605ff4854c3b715fb725627874c488
Bozakov <zdravko.bozakov@emc.com>
2016-05-23 17:09:59 +0100

    implemented README example

--- policy/neatpmd.py
+++ policy/neatpmd.py
@@ -8,44 +8,11 @@ from policy import PIB, NEATCandidate, NEATProperty, NEATPolicy, NEATRequest, NE
 POLICY_DIR = "pib/"
 
 if __name__ == "__main__":
-    pib = PIB()
-    pib.load_policies()
-
     cib = CIB()
-    cib.load_cib()
+    cib.load_cib('cib/example/')
+    pib = PIB()
 
-    print(pib)
-    print(cib)
-
-    C = {"name": "C", "description": "foo and bar", "priority": "0",
-         "match": {"requested": {'foo': 'bar'}},
-         "properties": {"immutable": {'foo3': 'bar3'}}}
-
-    p = NEATPolicy(name="dynamic")
-    p.match.update({"is_wired_interface": True}.items())
-    p.properties.update([("TCP_CC", "cubic", NEATProperty.IMMUTABLE), ("MTU", "9600")])
-
-    pib.register(p)
-
-    query = NEATRequest(requested={'remote_address': '23:::23:12', 'foo': 'bar'},
-                        immutable={'MTU': 9600, "is_wired_interface": True},
-                        informational={'low_latency': True})
-
-    # lookup CIB
-    code.interact(local=locals())
-    cib.lookup(query)
-    code.interact(local=locals(), banner='CIB lookup done')
-
-    nc = query.candidates[0]
-
-    query.dump()
-
-    pib.lookup_all(query.candidates)
-
-    code.interact(local=locals(), banner='PIB lookup done')
-    query.dump()
-
-    # ----- example from README -----
+    # ----- example from README.md -----
     request = NEATRequest()
     request.properties.insert(NEATProperty(('remote_ip', '10.1.23.45'), level=NEATProperty.IMMUTABLE))
     request.properties.insert(NEATProperty(('MTU', (1500, float('inf')))))
@@ -55,16 +22,24 @@ if __name__ == "__main__":
 
     policy1 = NEATPolicy(name='Bulk transfer')
     policy1.match.insert(NEATProperty(('remote_ip', '10.1.23.45')))
-    policy1.properties.insert(NEATProperty(('capacity', (10, 100))))
+    policy1.properties.insert(NEATProperty(('capacity', (10, 100)), level=NEATProperty.IMMUTABLE))
     policy1.properties.insert(NEATProperty(('MTU', 9600)))
+    pib.register(policy1)
 
     policy2 = NEATPolicy(name='TCP options')
     policy2.match.insert(NEATProperty(('MTU', 9600)))
     policy2.match.insert(NEATProperty(('is_wired', True)))
     policy2.properties.insert(NEATProperty(('TCP_window_scale', True)))
+    pib.register(policy2)
 
-    ###########
-    nc = NEATCandidate(query)
-    # for i in query.items():
-    #    nc.properties.add(NEATProperty(i))
-    c = cib.entries['s3']
+    #code.interact(local=locals(), banner='start')
+
+    print("PIB lookup:")
+    cib.lookup(request)
+    request.dump()
+    #code.interact(local=locals(), banner='CIB lookup done')
+
+    print("PIB lookup:")
+    pib.lookup_all(request.candidates)
+    request.dump()
+    #code.interact(local=locals(), banner='PIB lookup done')

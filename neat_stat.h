e52fc14f632d76d9ba4c92a92b86f9fff8321524
Andreas Petlund <apetlund@simula.no>
2016-05-30 15:29:33 +0200

    Minor fixes

--- neat_stat.h
+++ neat_stat.h
@@ -1,13 +1,16 @@
 #ifndef NEAT_STAT_H
 #define NEAT_STAT_H
 
+#include <stdint.h>
+
 // Struct for collecting global statistics
 struct neat_stat{
-  /* List of active flows */
+  uint32_t num_flows; // Number of active flows
+	
+  /* List of active flows This, I would presume belongs somewhere else*/
   //Remember to clean when a flow ends, cleanly or in other ways
-
-
-} 
+  
+};
 
 
 #endif

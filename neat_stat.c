b696ef6f8857924171c26a1f97bb002e27fe9cb5
Andreas Petlund <apetlund@simula.no>
2016-06-15 17:05:43 +0200

    Moved declaration to beginning of function and assignment to after the debug line in case sth bad happens on json_object()

--- neat_stat.c
+++ neat_stat.c
@@ -13,21 +13,23 @@
    then format the stats as a json string to return */	
 void neat_stats_build_json(neat_flow *flow, char **json_stats)
 {
-	json_t *root = json_object();
+	json_t *json_root;
 
 	neat_log(NEAT_LOG_DEBUG, "%s", __func__);
 
-	json_object_set_new( root, "remote_host", json_string( flow->name ));
-	json_object_set_new( root, "sock_type", json_integer( flow->sockType ));
-	json_object_set_new( root, "sock_protocol", json_integer( flow->sockProtocol ));
-	json_object_set_new( root, "port", json_integer( flow->port ));
+	json_root = json_object();
+
+	json_object_set_new( json_root, "remote_host", json_string( flow->name ));
+	json_object_set_new( json_root, "sock_type", json_integer( flow->sockType ));
+	json_object_set_new( json_root, "sock_protocol", json_integer( flow->sockProtocol ));
+	json_object_set_new( json_root, "port", json_integer( flow->port ));
 	
 	/* TODO: fetch OS-specific stats from the respective stacks used for connecting */
 
 	/* Callers must remember to free the output */
-	*json_stats = json_dumps(root, 0);
+	*json_stats = json_dumps(json_root, 0);
 
-	json_decref(root);
+	json_decref(json_root);
 
 	return;
 } 

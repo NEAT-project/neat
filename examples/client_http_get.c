a0d307ba2998fe89433685483cdabc5534a0fb60
Felix Weinrank <weinrank@fh-muenster.de>
2016-06-29 09:38:36 +0200

    client_http_get : add support for multiple streams

--- examples/client_http_get.c
+++ examples/client_http_get.c
@@ -1,20 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "../neat.h"
 
 /**********************************************************************
 
     HTTP-GET client in neat
-
-    client_http_get HOST [URI]
-
     * connect to HOST and send GET request
     * write response to stdout
+
+    client_http_get [OPTIONS] HOST
+    -u : URI
+    -n : number of requests/flows
 
 **********************************************************************/
 
 static uint32_t config_rcv_buffer_size = 1024;
+static uint32_t config_max_flows = 50;
 static char request[512];
 static const char *request_tail = "HTTP/1.0\r\nUser-agent: libneat\r\nConnection: close\r\n\r\n";
 
@@ -78,27 +81,45 @@ int
 main(int argc, char *argv[])
 {
     struct neat_ctx *ctx = NULL;
-    struct neat_flow *flow = NULL;
-    struct neat_flow_operations ops;
+    struct neat_flow *flows[config_max_flows];
+    struct neat_flow_operations ops[config_max_flows];
     uint64_t prop = 0;
-    int result;
+    int result = 0;
+    int arg = 0;
+    uint32_t num_flows = 1;
+    uint32_t i = 0;
+    result = EXIT_SUCCESS;
 
     memset(&ops, 0, sizeof(ops));
 
-    result = EXIT_SUCCESS;
-
-    if (argc < 2 || argc > 3) {
-        fprintf(stderr, "usage: neat_http_get HOST [URI]\n");
+    if (argc < 2 || argc > 4) {
+        fprintf(stderr, "usage: client_http_get [OPTIONS] HOST\n");
         result = EXIT_FAILURE;
         goto cleanup;
     }
 
-    if (argc == 3) {
-        snprintf(request, sizeof(request), "GET %s %s", argv[2], request_tail);
-    } else {
-        snprintf(request, sizeof(request), "GET %s %s", "/", request_tail);
+    snprintf(request, sizeof(request), "GET %s %s", "/", request_tail);
+
+    while ((arg = getopt(argc, argv, "u:n:")) != -1) {
+        switch(arg) {
+        case 'u':
+            snprintf(request, sizeof(request), "GET %s %s", optarg, request_tail);
+            break;
+        case 'n':
+            num_flows = strtoul (optarg, NULL, 0);
+            if (num_flows > config_max_flows) {
+                num_flows = config_max_flows;
+            }
+            fprintf(stderr, "%s - option - number of flows: %d\n", __func__, num_flows);
+            break;
+        default:
+            fprintf(stderr, "usage: client_http_get [OPTIONS] HOST\n");
+            goto cleanup;
+            break;
+        }
     }
-    printf("requesting: %s\n", request);
+
+    printf("%d flows - requesting: %s\n", num_flows, request);
 
     if ((ctx = neat_init_ctx()) == NULL) {
         fprintf(stderr, "could not initialize context\n");
@@ -106,32 +127,37 @@ main(int argc, char *argv[])
         goto cleanup;
     }
 
-    if ((flow = neat_new_flow(ctx)) == NULL) {
-        fprintf(stderr, "could not initialize context\n");
-        result = EXIT_FAILURE;
-        goto cleanup;
+
+    for (i = 0; i < num_flows; i++) {
+        if ((flows[i] = neat_new_flow(ctx)) == NULL) {
+            fprintf(stderr, "could not initialize context\n");
+            result = EXIT_FAILURE;
+            goto cleanup;
+        }
+
+        neat_get_property(ctx, flows[i], &prop);
+        prop |= NEAT_PROPERTY_OPTIONAL_SECURITY;
+        prop |= NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED;
+        neat_set_property(ctx, flows[i], prop);
+
+        ops[i].on_connected = on_connected;
+        ops[i].on_error = on_error;
+        neat_set_operations(ctx, flows[i], &(ops[i]));
+
+        // wait for on_connected or on_error to be invoked
+        if (neat_open(ctx, flows[i], argv[argc - 1], 80) != NEAT_OK) {
+            fprintf(stderr, "Could not open flow\n");
+            result = EXIT_FAILURE;
+        }
     }
 
-    neat_get_property(ctx, flow, &prop);
-    prop |= NEAT_PROPERTY_OPTIONAL_SECURITY;
-    prop |= NEAT_PROPERTY_RETRANSMISSIONS_REQUIRED;
-    neat_set_property(ctx, flow, prop);
-
-    ops.on_connected = on_connected;
-    ops.on_error = on_error;
-    neat_set_operations(ctx, flow, &ops);
-
-    // wait for on_connected or on_error to be invoked
-    if (neat_open(ctx, flow, argv[1], 80) == NEAT_OK)
-        neat_start_event_loop(ctx, NEAT_RUN_DEFAULT);
-    else {
-        fprintf(stderr, "Could not open flow\n");
-        result = EXIT_FAILURE;
-    }
+    neat_start_event_loop(ctx, NEAT_RUN_DEFAULT);
 
 cleanup:
-    if (flow != NULL) {
-        neat_free_flow(flow);
+    for (i = 0; i < num_flows; i++) {
+        if (flows[i] != NULL) {
+            neat_free_flow(flows[i]);
+        }
     }
     if (ctx != NULL) {
         neat_free_ctx(ctx);

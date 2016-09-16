43de8a09b9ef35cb5e639a10555cc03d9959bf01
Andreas Petlund <apetlund@simula.no>
2016-09-16 14:51:40 +0200

    Now looping through flows

--- neat_stat.c
+++ neat_stat.c
@@ -14,28 +14,27 @@
 void neat_stats_build_json(struct neat_ctx *mgr, char **json_stats)
 {
 	json_t *json_root;
-    struct neat_flow *flow_ptr;
+    struct neat_flow *flow;
+    int flowcount;
 
 	neat_log(NEAT_LOG_DEBUG, "%s", __func__);
 
-
-    flow_ptr = LIST_FIRST(&(mgr->flows));
-
-//    /* Traverse the list of slows and get statistics for each flow  + global stats*/
-//    for (flow_ptr = LIST_FIRST(&mgr->flows); flow_ptr != NULL; flow_ptr=flow_nxt) {
-//        flow_nxt = LIST_NEXT(flow_ptr, entry);
-//        printf("Flow: %i\n", flow_ptr->port); 
-//        }
-//    }
-
+    flowcount = 0;
 	json_root = json_object();
 
-	json_object_set_new( json_root, "remote_host", json_string( flow_ptr->name ));
-	//json_object_set_new( json_root, "sock_type", json_integer( flow->socket->type ));
-	//json_object_set_new( json_root, "sock_protocol", json_integer( neat_stack_to_protocol(flow->socket->stack) ));
-	//json_object_set_new( json_root, "port", json_integer( flow->port ));
+    LIST_FOREACH(flow, &mgr->flows, next_flow) {
+        flowcount++;
+	    json_object_set_new( json_root, "remote_host", json_string( flow->name ));
+        json_object_set_new( json_root, "sock_type", json_integer( flow->socket->type ));
+        json_object_set_new( json_root, "sock_protocol", json_integer( neat_stack_to_protocol(flow->socket->stack) ));
+        json_object_set_new( json_root, "port", json_integer( flow->port ));
+    }
 
-	/* TODO: fetch OS-specific stats from the respective stacks used for connecting */
+    json_object_set_new( json_root, "Number of flows", json_integer( flowcount ));
+
+
+	/* TODO: fetch OS-specificon_object_set_new( json_root, "port", json_integer( flow->port ));
+     * stats from the respective stacks used for connecting */
 
 	/* Callers must remember to free the output */
 	*json_stats = json_dumps(json_root, 0);

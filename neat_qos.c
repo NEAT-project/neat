7e7ce56ffba98c44d259a14d4a5b8b6dfda339ae
Tom Jones <jones@sdf.org>
2016-09-20 10:44:32 +0100

    Add sctp support for qos setting

--- neat_qos.c
+++ neat_qos.c
@@ -4,6 +4,11 @@
 #include "neat.h"
 #include "neat_qos.h"
 
+#if defined(USRSCTP_SUPPORT)          
+    #include "neat_usrsctp_internal.h"
+    #include <usrsctp.h>              
+#endif                                
+
 uint8_t 
 neat_map_qos_to_dscp(uint8_t qos)
 {
@@ -18,13 +23,33 @@ neat_error_code
 neat_set_tos(struct neat_ctx *ctx, struct neat_flow *flow)
 {
     uint8_t dscp; 
-	int tos;
+    int tos;
+
+    dscp = neat_map_qos_to_dscp(flow->qos);
+    tos = dscp | flow->ecn;
 
     switch (flow->socket->stack) {
-    case NEAT_STACK_UDP:
-        dscp = neat_map_qos_to_dscp(flow->qos);
-        tos = dscp | flow->ecn;
+#if defined( SCTP_PEER_ADDR_PARAMS)
+    case NEAT_STACK_SCTP:
+        struct sctp_paddrparms params;
+        params.spp_dscp = dscp;
+        params.spp_flags = SPP_DSCP;
 
+#if defined(USRSCTP_SUPPORT)  
+        if(usrsctp_setsockopt(flow->socket->fd, 
+            IPPROTO_SCTP, SCTP_PEER_ADDR_PARAMS, &params, sizeof(params)) == -1) {
+            return NEAT_ERROR_UNABLE;
+        }
+#else
+        if(setsockopt(flow->socket->fd, 
+            IPPROTO_SCTP, SCTP_PEER_ADDR_PARAMS, &params, sizeof(params)) == -1) {
+            return NEAT_ERROR_UNABLE;
+        }
+#endif //USRSCTP
+
+        return NEAT_OK;
+#endif //SCTP_PEER_ADDR_PARAMS
+    case NEAT_STACK_UDP:
         if(setsockopt(flow->socket->fd, 
             IPPROTO_IP, IP_TOS, &tos, sizeof(tos)) == -1) {
             return NEAT_ERROR_UNABLE;

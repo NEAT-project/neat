6f7693ab5e7ca2b3aa41d69eadb5ef4e613b2e7c
Felix Weinrank <weinrank@fh-muenster.de>
2016-09-20 11:35:26 +0200

    refactor test script

--- tests/run.sh
+++ tests/run.sh
@@ -6,14 +6,16 @@ echo "########################################"
 PREFIX=$1
 EXAMPLES_DIR="../examples"
 RC_GLOBAL=0
+ARG=""
 
 ##############
 
 echo ""
 echo "HTTP-Client requesting from bsd10"
 echo "########################################"
-$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he bsd10.nplab.de
-echo ""
+ARG="$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he bsd10.nplab.de"
+echo "Running: $ARG"
+$ARG
 RC=$?
 if [[ $RC != 0 ]]; then
     RC_GLOBAL=1
@@ -27,8 +29,9 @@ fi
 echo ""
 echo "HTTP-Client requesting from bsd10"
 echo "########################################"
-$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he 212.201.121.100
-echo ""
+ARG="$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he 212.201.121.100"
+echo "Running: $ARG"
+$ARG
 RC=$?
 if [[ $RC != 0 ]]; then
     RC_GLOBAL=1
@@ -42,8 +45,9 @@ fi
 echo ""
 echo "HTTP-Client requesting from bsd10"
 echo "########################################"
-$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he 2a02:c6a0:4015:10::100
-echo ""
+ARG="$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he 2a02:c6a0:4015:10::100"
+echo "Running: $ARG"
+$ARG
 RC=$?
 if [[ $RC != 0 ]]; then
     RC_GLOBAL=1
@@ -57,9 +61,10 @@ fi
 echo ""
 echo "HTTP-Client requesting from bsd10"
 echo "########################################"
-$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he not.resolvable.neat
+ARG="$PREFIX$EXAMPLES_DIR/client_http_get -u /cgi-bin/he not.resolvable.neat"
+echo "Running: $ARG"
+$ARG
 RC=$?
-echo ""
 if [[ $RC != 1 ]]; then
     RC_GLOBAL=1
     echo ">> RC $RC - test failed!"
@@ -67,4 +72,4 @@ else
     echo ">> RC $RC - test succeeded"
 fi
 
-return $RC_GLOBAL
+exit $RC_GLOBAL

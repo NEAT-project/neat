09abcd42d4205f8a8e81e7e7a751980311241d43
Øystein Dale <oystedal@ifi.uio.no>
2016-09-27 14:38:27 +0200

    Removed unused structs

--- neat_pm_socket.h
+++ neat_pm_socket.h
@@ -18,16 +18,6 @@ typedef void (*pm_callback)(struct neat_ctx *ctx, struct neat_flow *flow);
 typedef void (*pm_reply_callback)(struct neat_ctx *ctx, struct neat_flow *flow, json_t *json);
 typedef void (*pm_error_callback)(struct neat_ctx *ctx, struct neat_flow *flow, int error);
 
-struct neat_pm_context {
-    uv_pipe_t pm_pipe;
-    // uv_stream_t *pm_handle;
-    // char *pm_path;
-};
-
-struct neat_pm_request {
-    uv_handle_t handle;
-};
-
 // neat_error_code neat_pm_socket_connect(struct neat_ctx *ctx, struct neat_flow *flow, pm_callback cb);
 neat_error_code neat_pm_send(struct neat_ctx *ctx, struct neat_flow *flow, char *buffer, pm_reply_callback cb, pm_error_callback err_cb);
 

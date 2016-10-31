3b4279e43d38035e70633f5bc4f7b1650213340e
Øystein Dale <oystedal@ifi.uio.no>
2016-10-31 21:42:47 +0100

    core: Reflect changes to neat_free_flow in neat_free_ctx

--- neat_core.c
+++ neat_core.c
@@ -233,7 +233,7 @@ static void neat_core_cleanup(struct neat_ctx *nc)
 //TODO: Consider adding callback, like for resolver
 void neat_free_ctx(struct neat_ctx *nc)
 {
-    struct neat_flow *f, *prev = NULL;
+    struct neat_flow *f;
     neat_log(NEAT_LOG_DEBUG, "%s", __func__);
 
     if (nc->resolver) {
@@ -242,17 +242,7 @@ void neat_free_ctx(struct neat_ctx *nc)
 
     while (!LIST_EMPTY(&nc->flows)) {
         f = LIST_FIRST(&nc->flows);
-
-        /*
-         * If this assert triggers, it means that a call to neat_free_flow did
-         * not remove the flow pointed to by f from the list of flows. The
-         * assert is present because clang-analyzer somehow doesn't see the fact
-         * that the list is changed in synchronous_free().
-         */
-        assert(f != prev);
-
         neat_free_flow(f);
-        prev = f;
     }
 
     neat_core_cleanup(nc);

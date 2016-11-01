eae31957a522ed70a8c67e243297f5afa1c1b172
Øystein Dale <oystedal@ifi.uio.no>
2016-11-01 14:18:29 +0100

    test: Update test_echo.sh

--- tests/test_echo.sh
+++ tests/test_echo.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
 
-# determine script directory
-DIR=$(dirname "$0")
-
-# run server and use first script argument as prefix - e.g. "valgrind"
-($1 $DIR/../examples/server_echo -P "") &
-SERVER_PID=$!
+# TODO: Not running with $PREFIX here.
+# server_echo should capture a signal and stop the event loop
+# to allow for graceful shutdown.
+ARG="$EXAMPLES_DIR/server_echo -P $EXAMPLES_DIR/all.json"
 echo "Starting NEAT server..."
+$ARG &
+SERVER_PID=$!
 
 # wait until server started
 sleep 3
 
 # check if the server process is runnning
-kill -0 $!
+kill -0 $SERVER_PID
 res=$?
 if [ $res -ne 0 ]; then
     echo "Server not running - exit"
     exit 1
-fi
-
-# run the tests
-echo "Starting tests..."
-$1 $DIR/test_echo
-res=$?
-echo "Tests finished"
-
-# graceful kill for server process and wait for output
-kill -TERM $SERVER_PID
-sleep 2
-# kill it with fire...
-kill -KILL $SERVER_PID
-
-if [ $res -ne 0 ]; then
-    echo "FAILED"
-    exit 1
 else
-    echo "SUCCESS"
-    exit 0
+
+    # run the tests
+    echo "Starting tests..."
+    ./test_echo
+    res=$?
+    echo "Tests finished"
+
+    # graceful kill for server process and wait for output
+    kill -TERM $SERVER_PID
+    sleep 3
+    # kill it with fire...
+    kill -KILL $SERVER_PID
+
+    if [ $res -ne 0 ]; then
+        echo "FAILED"
+        exit 1
+    else
+        echo "SUCCESS"
+        exit 0
+    fi
 fi
+

eb84f5bed11681ba86823cc9c5d61ac99f7909c8
Øystein Dale <oystedal@ifi.uio.no>
2016-11-01 14:16:08 +0100

    test: Add testing of echo server to default tests

--- tests/run.sh
+++ tests/run.sh
@@ -70,6 +70,49 @@ fi
 
 ##############
 
+# TODO: Not running with $PREFIX here.
+# server_echo should capture a signal and stop the event loop
+# to allow for graceful shutdown.
+echo ""
+echo "########################################"
+ARG="$EXAMPLES_DIR/server_echo -P $EXAMPLES_DIR/all.json"
+echo "Starting NEAT server..."
+$ARG &
+SERVER_PID=$!
+
+# wait until server started
+sleep 3
+
+# check if the server process is runnning
+kill -0 $SERVER_PID
+res=$?
+if [ $res -ne 0 ]; then
+    echo "Server not running - exit"
+    # exit 1
+else
+
+    # run the tests
+    echo "Starting tests..."
+    ./test_echo
+    RC=$?
+    echo "Tests finished"
+
+    # graceful kill for server process and wait for output
+    kill -TERM $SERVER_PID
+    sleep 3
+    # kill it with fire...
+    kill -KILL $SERVER_PID
+
+    if [ $RC -ne 0 ]; then
+        echo ">> RC $RC - test failed!"
+    else
+        echo ">> RC $RC - test succeeded"
+    fi
+fi
+
+
+##############
+
 if [ ! -n "$PREFIX" ]; then
     echo ""
     echo "########################################"

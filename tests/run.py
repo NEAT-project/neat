e486f643ac7e6523877937f29253d14cf6193786
Felix Weinrank <weinrank@fh-muenster.de>
2016-12-09 23:46:52 +0100

    bb - improvements

--- tests/run.py
+++ tests/run.py
@@ -2,18 +2,21 @@
 import subprocess
 import sys
 
+
+# Test syntax ([expected return value], [test status: 0-not executed yet / 1-passed / 2-failed])
+
 # General tests
 tests_general = []
-tests_general.append([0, 'client_http_get -u /cgi-bin/he -v 2 bsd10.nplab.de'])
-tests_general.append([0, 'client_http_get -u /cgi-bin/he -v 2 212.201.121.100'])
-tests_general.append([0, 'client_http_get -u /cgi-bin/he -v 2 2a02:c6a0:4015:10::100'])
-tests_general.append([1, 'client_http_get -u /cgi-bin/he -v 2 not.resolvable.neat'])
-tests_general.append([1, 'client_http_get -u /cgi-bin/he -v 2 buildbot.nplab.de'])
-tests_general.append([1, 'client_http_run_once -u /cgi-bin/he bsd10.nplab.de'])
+tests_general.append([0, 0, 'client_http_get -u /cgi-bin/he -v 2 bsd10.nplab.de'])
+tests_general.append([0, 0, 'client_http_get -u /cgi-bin/he -v 2 212.201.121.100'])
+tests_general.append([0, 0, 'client_http_get -u /cgi-bin/he -v 2 2a02:c6a0:4015:10::100'])
+tests_general.append([1, 0, 'client_http_get -u /cgi-bin/he -v 2 not.resolvable.neat'])
+tests_general.append([1, 0, 'client_http_get -u /cgi-bin/he -v 2 buildbot.nplab.de'])
+tests_general.append([0, 0, 'client_http_run_once -u /cgi-bin/he bsd10.nplab.de'])
 
 # USRSCTP specific tests
 tests_usrsctp = []
-tests_usrsctp.append([0, 'client_http_get -u /cgi-bin/he -v 2 bsd10.nplab.de'])
+tests_usrsctp.append([0, 0,'client_http_get -u /cgi-bin/he -v 2 bsd10.nplab.de'])
 
 # Default values
 tests       = tests_general
@@ -38,24 +41,38 @@ if len(sys.argv) > 2 :
 print("prefix : " + prefix)
 print("Starting tests...")
 
+result_global = 0
+
 # Iteratre through tests
 for test in tests:
-    test_return = 0
-    print("Runnning: " + test[1])
+    result = 0
+    print("Runnning: " + test[2])
     try:
-        test_return = subprocess.call(workdir + test[1], shell=True, timeout=40)
-        if test_return != test[0]:
+        result = subprocess.call(workdir + test[2], shell=True, timeout=40)
+        if result != test[0]:
             print("Test failed: program returned with error")
-            sys.exit(-1)
+            test[1] = 2
+            break
     except subprocess.TimeoutExpired:
         print("Test failed: timeout")
-        sys.exit(-1)
+        test[1] = 2
+        break
     except:
         print("Something went wrong")
-        sys.exit(-1)
+        test[1] = 2
+        break
 
-    print(test[1] + " >> returned " + str(test_return))
+    # test succeeded
+    test[1] = 1
 
 
-print("Tests finished")
+print("Tests finished - summary")
+for test in tests:
+    if test[1] == 1:
+        print("PASSED   --  " + test[2])
+    elif test[1] == 2:
+        print("FAILED   --  " + test[2])
+    else:
+        print("SKIPPED  --  " + test[2])
+
 sys.exit(0)

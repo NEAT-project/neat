6b2443a7fbdcb8adad4e2f2eaf04bb2a0e27852b
Thomas Dreibholz <dreibh@simula.no>
2016-12-20 10:53:23 +0100

    Got rid of extremely verbose logging.

--- socketapi/neat-socketapi-internals.c
+++ socketapi/neat-socketapi-internals.c
@@ -125,6 +125,8 @@ struct neat_socketapi_internals* nsa_initialize()
       gSocketAPIInternals->nsi_socket_identifier_bitmap = ibm_new(FD_SETSIZE);
       if(gSocketAPIInternals->nsi_socket_identifier_bitmap != NULL) {
          /* ====== NEAT context ========================================== */
+         neat_log_level(NEAT_LOG_ERROR);   /* This may be improved ... */
+
          gSocketAPIInternals->nsi_neat_context = neat_init_ctx();
          if(gSocketAPIInternals->nsi_neat_context != NULL) {
             /* ====== Map stdin, stdout, stderr file descriptors ========= */

f0b9c2d790e0d98213926e23282834dfaf4e5fd0
Simone Mangiante <simone.mangiante@emc.com>
2017-01-25 12:07:28 +0000

    added commit scripts

---
+++ policy/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+
+if sys.version_info < (3, 5):
+    sys.exit("Python 3.5 is required")
+
+setup(name='neatpmd',
+      version='0.1',
+      description='NEAT Policy Manager Daemon',
+      author='Zdravko Bozakov',
+      author_email='zdravko@bozakov.de',
+      url='https://github.com/NEAT-project/neat/tree/master/policy/',
+      scripts=['neatpmd'],
+      py_modules=['policy', 'cib', 'pib', 'pmdefaults', 'pmhelper', 'pmrest'],
+     )
+
+

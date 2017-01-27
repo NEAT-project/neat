9192a5ca396836c1a61033eb59e1a83b1fb33c20
Per Hurtig <per.hurtig@kau.se>
2017-01-27 10:44:02 +0100

    improved error detection and mixed mode (e.g. SOL_TCP/6) input from policies

--- policy/pmhelper.py
+++ policy/pmhelper.py
@@ -4,20 +4,23 @@ import socket
 so_separator = '/'
 so_prefix = 'SO'
 
-# for testing
-so_str = 'SO/SOL_TCP/SO_KEEPALIVE'
-
-
 def sock_prop(so_str):
     if not so_str.startswith(so_prefix + so_separator):
         return
     _, level, optname = so_str.split(so_separator)
+    if level.isdigit() and optname.isdigit():
     try:
         # get socket level
-        sol_i = getattr(socket, level)
-        so_i = getattr(socket, optname)
+        if level.isdigit():
+            sol_i = level
+        else:
+            sol_i = getattr(socket, level)
+        if optname.isdigit():
+            so_i = optname
+        else:
+            so_i = getattr(socket, optname)
     except AttributeError as e:
         logging.debug('Unknown socket option' + e.args[0])
-        return
+        return -1
 
     return so_separator.join(('SO', str(sol_i), str(so_i)))

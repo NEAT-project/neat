NEAT-project/neat

commit 212adffde7109119710319fb92c56ec3f18fd12f

open_resolve_cb: handle zero nr_of_stacks better
This situation (no protocol stacks to use) cannot be handled with
assert() as it is "normal" error scenario that we need to bail out from
nicely.

neat_core.c
@@ -3049,6 +3049,8 @@ open_resolve_cb(struct neat_resolver_results *results, uint8_t code,
     struct neat_resolver_res *result;
     struct neat_he_candidates *candidates;
 
+    assert(results);
+
     neat_log(ctx, NEAT_LOG_DEBUG, "%s", __func__);
 
     if (code != NEAT_RESOLVER_OK) {
@@ -3059,8 +3061,10 @@ open_resolve_cb(struct neat_resolver_results *results, uint8_t code,
     // Find the enabled stacks based on the properties
     // nr_of_stacks = neat_property_translate_protocols(flow->propertyAttempt, stacks);
     neat_find_enabled_stacks(flow->properties, stacks, &nr_of_stacks, NULL);
-    assert(nr_of_stacks);
-    assert(results);
+    if (!nr_of_stacks) {
+        neat_io_error(ctx, flow, NEAT_ERROR_UNABLE);
+        return NEAT_ERROR_UNABLE;
+    }
 
     flow->resolver_results = results;
 

commit 559cb3cf98a3833221bf5719f5653f5c72359dec

open_resolve_cb: regression: move back assert()
212adffde710 broke some tests

neat_core.c
@@ -3050,8 +3050,6 @@ open_resolve_cb(struct neat_resolver_results *results, uint8_t code,
     struct neat_resolver_res *result;
     struct neat_he_candidates *candidates;
 
-    assert(results);
-
     neat_log(ctx, NEAT_LOG_DEBUG, "%s", __func__);
 
     if (code != NEAT_RESOLVER_OK) {
@@ -3066,6 +3064,8 @@ open_resolve_cb(struct neat_resolver_results *results, uint8_t code,
         neat_io_error(ctx, flow, NEAT_ERROR_UNABLE);
         return NEAT_ERROR_UNABLE;
     }
+
+    assert(results);
 
     flow->resolver_results = results;
 
cb8d0a9be38168c0fa9c2993f5b4edb1a6294a93
Zdravko Bozakov <zdravko.bozakov@dell.com>
2017-03-31 12:56:34 +0100

    handle invalid PM requests

--- policy/debian/99-neat-vars.sh
+++ policy/debian/99-neat-vars.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# This script may be placed in /etc/profile.d to globally export the
+# NEAT Policy manager domain socket directory.
+
 SOCKDIR="/var/run/neat"
 export NEAT_PM_SOCKET=$SOCKDIR/neat_pm_socket
--- policy/debian/neat-motd.sh
+++ policy/debian/neat-motd.sh
@@ -17,4 +17,5 @@ echo
 echo 'NEAT Policy Manager'
 echo 'Usage: service neatpmd {start|stop|status|restart|uninstall}'
 /etc/init.d/neatpmd status
-echo 'logging to /var/log/neatpm.log'
+echo 'Log: /var/log/neatpm.log'
+

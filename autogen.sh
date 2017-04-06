b1f93cf628ee57826811f8b85fd83e9bf0245495
Thomas Dreibholz <dreibh@simula.no>
2017-04-06 13:19:51 +0200

    Build package with libusrsctp.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 rm -f CMakeCache.txt
-cmake -DCMAKE_INSTALL_PREFIX=/usr .
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DUSRSCTP_SUPPORT=1 -DSCTP_MULTISTREAMING=1 -DFLOW_GROUPS=1 .
 
 cores=`getconf _NPROCESSORS_ONLN`
 make -j${cores}
--- rpm/libneat.spec
+++ rpm/libneat.spec
@@ -1,5 +1,5 @@
 Name: libneat
-Version: 0.0.1~td133
+Version: 0.0.1~td134
 Release: 1
 Summary: NEAT Project
 License: BSD

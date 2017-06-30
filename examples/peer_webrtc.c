a33612160d394bf679f0518932e711550212f0eb
Felix Weinrank <weinrank@fh-muenster.de>
2017-06-30 10:37:46 +0200

    webrtc - re-enable neat_send_remote_parameters

--- examples/peer_webrtc.c
+++ examples/peer_webrtc.c
@@ -103,10 +103,13 @@ static neat_error_code
 on_parameters(struct neat_flow_operations *opCB)
 {
     fprintf(stderr, "%s\n", __func__);
-
+    char *remote = NULL;
     printf("LocalParameters: %s\n", (char *)opCB->userData);
     printf("Got local parameters from WebRTC. Now send them to signalling server\n");
-
+    remote = strdup((char *)opCB->userData);
+    if (neat_send_remote_parameters(opCB->ctx, opCB->flow, remote) != NEAT_OK) {
+        free(remote);
+    }
     free(opCB->userData);
     opCB->userData = NULL;
     return NEAT_OK;

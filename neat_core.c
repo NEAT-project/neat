78c99083282721214ed71a7dbade2f69af8d45e6
Zdravko Bozakov <zdravko.bozakov@dell.com>
2017-09-19 10:25:34 +0100

    fixed hard coded HE result

--- neat_core.c
+++ neat_core.c
@@ -1981,7 +1981,7 @@ send_result_connection_attempt_to_pm(neat_ctx *ctx, neat_flow *flow, struct cib_
         "transport", "value", stack_to_string(he_res->transport ),
         "remote_ip", "value", he_res->remote_ip,
         "remote_port", "value", he_res->remote_port,
-        "cached", "value", 1, "precedence", 2, "score", 5);
+        "cached", "value", result, "precedence", 2, "score", 5);
     if (prop_obj == NULL) {
         goto end;
     }

4fb03c8ae15e1777d4db6ea0afa9d1003c74e2aa
Thomas Dreibholz <dreibh@iem.uni-due.de>
2017-09-22 20:29:38 +0200

    Added missing wrappers nsa_lseek() and nsa_lseek64().

--- socketapi/neat-socketapi.c
+++ socketapi/neat-socketapi.c
@@ -818,6 +818,34 @@ int nsa_creat(const char* pathname, mode_t mode)
 }
 
 
+/* ###### NEAT lseek() implementation #################################### */
+off_t nsa_lseek(int fd, off_t offset, int whence);
+{
+   GET_NEAT_SOCKET(fd)
+   if(neatSocket->ns_flow != NULL) {
+      errno = EOPNOTSUPP;
+      return(-1);
+   }
+   else {
+      return(lseek(neatSocket->ns_socket_sd, offset, whence));
+   }
+}
+
+
+/* ###### NEAT lseek64() implementation ################################## */
+off64_t nsa_lseek64(int fd, off64_t offset, int whence);
+{
+   GET_NEAT_SOCKET(fd)
+   if(neatSocket->ns_flow != NULL) {
+      errno = EOPNOTSUPP;
+      return(-1);
+   }
+   else {
+      return(lseek64(neatSocket->ns_socket_sd, offset, whence));
+   }
+}
+
+
 /* ###### NEAT ioctl() implementation #################################### */
 int nsa_ioctl(int fd, int request, const void* argp)
 {

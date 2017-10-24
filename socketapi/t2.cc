f0e038fea50d7080ed8804c1cd63c16a147b7fc5
Thomas Dreibholz <dreibh@iem.uni-due.de>
2017-10-24 17:59:00 +0200

    Added test.

--- socketapi/t2.cc
+++ socketapi/t2.cc
@@ -1,50 +1,39 @@
+#include <neat-socketapi.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <assert.h>
-#include <sys/queue.h>
-
-
-struct event_signal_node
-{
-   int num;
-   TAILQ_ENTRY(event_signal_node) esn_node;
-};
-
-struct event_signal
-{
-   bool            es_has_fired;
-   TAILQ_HEAD(slisthead, event_signal_node) es_parent_list;
-};
 
 
 int main(int argc, char** argv)
 {
-   event_signal es;
-   TAILQ_INIT(&es.es_parent_list);
+   int sd = nsa_open("/dev/random", O_RDONLY, 0);
+   if(sd >= 0) {
+      while(1) {
+         char buffer[16];
 
-   struct event_signal_node* n1 = (struct event_signal_node*)malloc(sizeof(struct event_signal_node));
-   n1->num=1;
-   TAILQ_INSERT_TAIL(&es.es_parent_list, n1, esn_node);
+         pollfd pfd;
+         pfd.fd     = sd;
+         pfd.events = POLLIN;
 
-   struct event_signal_node* n2 = (struct event_signal_node*)malloc(sizeof(struct event_signal_node));
-   n2->num=2;
-   TAILQ_INSERT_TAIL(&es.es_parent_list, n2, esn_node);
-
-   struct event_signal_node* n3 = (struct event_signal_node*)malloc(sizeof(struct event_signal_node));
-   n3->num=3;
-   TAILQ_INSERT_TAIL(&es.es_parent_list, n3, esn_node);
-
-
-   struct event_signal_node* np;
-   TAILQ_FOREACH(np, &es.es_parent_list, esn_node) {
-      printf("N=%d\n", np->num);
+         int p = nsa_poll(&pfd, 1, 1000);
+         if(p > 0) {
+            ssize_t r = nsa_read(sd, (char*)&buffer, sizeof(buffer));
+            if(r <= 0) {
+               perror("nsa_read()");
+               break;
+            }
+            printf("r=%d\n", (int)r);
+         }
+         else {
+            printf("timeout\n");
+         }
+      }
    }
 
-   while( (np = TAILQ_FIRST(&es.es_parent_list)) != NULL ) {
-      TAILQ_REMOVE(&es.es_parent_list, np, esn_node);
-      free(np);
-   }
-
+   nsa_close(sd);
+   nsa_cleanup();
    return 0;
 }

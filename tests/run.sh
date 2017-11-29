4f468d03fb677013ca5ba63e0ec95a58de2a2866
Felix Weinrank <weinrank@fh-muenster.de>
2017-11-29 17:46:31 +0100

    better testing

--- tests/run.sh
+++ tests/run.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-set -e
 
+retcode=1
+cmdprefix=""
 
-declare -a arr=("../examples/client_http_get -u /cgi-bin/he -v 2 bsd10.nplab.de"
-                "../examples/client_http_get -u /cgi-bin/he -v 2 212.201.121.100"
-                "../examples/client_http_get -u /cgi-bin/he -v 2 2a02:c6a0:4015:10::100"
-                "../examples/client_http_get -u /cgi-bin/he -v 2 buildbot.nplab.de"
-                "../examples/client_http_get -u /cgi-bin/he -v 2 not.resolvable.neat")
+if [ $# -eq 1 ]; then
+    cmdprefix=$1
+    echo "No arguments supplied"
+fi
 
-## now loop through the above array
-for i in "${arr[@]}"
-do
-    echo "############################"
-    echo "$i"
-    echo ""
-    $i
-    echo ""
-    echo ""
-done
+function runtest {
+    $cmdprefix "$@"
+    local status=$?
+    if [ $status -ne $retcode ]; then
+        echo "TEST FAILED!" >&2
+        echo "$@" >&2
+        exit 0
+    fi
+    return $status
+}
+
+retcode=0
+runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "bsd10.nplab.de"
+runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "212.201.121.100"
+runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "2a02:c6a0:4015:10::100"
+
+retcode=1
+runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "buildbot.nplab.de"
+runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "not.resolvable.neat"
+
+unamestr=`uname`
+if [ "$unamestr" == "Linux" ] || [ "$unamestr" == "FreeBSD" ]; then
+    retcode=0
+    runtest "../examples/client_http_get" "-P" "../examples/prop_tcp_security.json" "-p" "443" "-v" "2" "www.fh-muenster.de"
+    runtest "../examples/tneat" "-v" "2" "-P" "../examples/prop_sctp_dtls.json" "interop.fh-muenster.de"
+    runtest "../examples/tneat" "-v" "2" "-L" "-n" "1024" "-P" "../examples/prop_sctp.json"
+fi

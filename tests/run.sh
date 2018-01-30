965e9126dc01dad40151d9aec47387c137988913
Felix Weinrank <weinrank@fh-muenster.de>
2018-01-30 10:05:06 +0100

    sync

--- tests/run.sh
+++ tests/run.sh
@@ -27,7 +27,7 @@ retcode=0
 runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "2" "interop.nplab.de"
 runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "1" "212.201.121.80"
 runtest "../examples/client_http_get" "-u" "/cgi-bin/he" "-v" "1" "2a02:c6a0:4015:11::80"
-runtest "../examples/tneat" "-L -v 3"
+runtest "../examples/tneat" "-L" "-v" "3"
 runtest "../examples/tneat" "-L" "-P" "../examples/prop_tcp_delayed.json"
 
 # Tests which should fail

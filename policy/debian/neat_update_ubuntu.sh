094133b0fcab0caee6df7435b4251bce3485591a
Zdravko Bozakov <zdravko.bozakov@dell.com>
2018-05-28 11:06:40 +0100

    updated some VM scripts

--- policy/debian/neat_update_ubuntu.sh
+++ policy/debian/neat_update_ubuntu.sh
@@ -2,10 +2,10 @@
 
 NEAT_DIR=~/neat
 
-mkdir -p NEAT_DIR/build
-cd NEAT_DIR/neat
+mkdir -p $NEAT_DIR/build
+cd $NEAT_DIR/neat
 git pull
-cd NEAT_DIR/build
+cd $NEAT_DIR/build
 cmake ..
 cmake --build .
 sudo make install
@@ -18,5 +18,5 @@ sudo cp -v $NEAT_DIR/policy/debian/neat-motd.sh /etc/update-motd.d/01-neat
 sudo service neatpmd restart
 /etc/init.d/neatpmd status
 
-sudo cp /home/neat/neat/policy/debian/services/neat_http_server.service /lib/systemd/system
+sudo cp $NEAT_DIR/policy/debian/services/neat_http_server.service /lib/systemd/system
 

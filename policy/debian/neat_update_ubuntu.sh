18fac0a4160fb909a74f980a7748e6d1c1e26287
Zdravko Bozakov <zdravko.bozakov@dell.com>
2018-05-28 11:05:23 +0100

    updated some VM scripts

--- policy/debian/neat_update_ubuntu.sh
+++ policy/debian/neat_update_ubuntu.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-mkdir -p ~/neat/build
-cd ~/neat
+NEAT_DIR=~/neat
+
+mkdir -p NEAT_DIR/build
+cd NEAT_DIR/neat
 git pull
-cd ~/neat/build
+cd NEAT_DIR/build
 cmake ..
 cmake --build .
 sudo make install
 
-sudo cp -v ~/neat/policy/debian/99-neat-vars.sh /etc/profile.d/
+sudo cp -v $NEAT_DIR/policy/debian/99-neat-vars.sh /etc/profile.d/
 sudo chmod +x /etc/profile.d/99-neat-vars.sh
-sudo cp -v ~/neat/policy/examples/pib/* /etc/neat/pib/
+sudo cp -v $NEAT_DIR/policy/examples/pib/* /etc/neat/pib/
+sudo cp -v $NEAT_DIR/policy/debian/neat-motd.sh /etc/update-motd.d/01-neat
 
 sudo service neatpmd restart
 /etc/init.d/neatpmd status

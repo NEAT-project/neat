85ae6ed007b216ca3afd3b47c77f1e6d078e2f2d
Thomas Dreibholz <dreibh@simula.no>
2018-06-07 12:14:46 +0200

    Updated NEAT Sockets API and corresponding examples.

--- socketapi/examples/httpserver2-select.cc
+++ socketapi/examples/httpserver2-select.cc
@@ -36,21 +36,11 @@
 
 
 static const char* properties = "{\
-    \"transport\": [\
-        {\
-            \"value\": \"SCTP\",\
-            \"precedence\": 1\
-        },\
-        {\
-            \"value\": \"MPTCP\",\
-            \"precedence\": 1\
-        },\
-        {\
-            \"value\": \"TCP\",\
-            \"precedence\": 1\
-        }\
-    ]\
-}";\
+   \"transport\": {\
+      \"value\": [ \"MPTCP\", \"SCTP\", \"SCTP/UDP\", \"TCP\" ],\
+      \"precedence\": 1\
+   }\
+}";
 
 
 void handleHTTPCommand(int sd, const unsigned int id, char* command)
